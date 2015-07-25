6b1836a1f5ec6551bda85328a3947a4c633e9b64
Ben Scott <ben@belfryimages.com.au>
2015-07-25 11:22:19 +1000

    Fix #1 better handling of getting expression field names

---
+++ src/Checkk.Tests/CheckAgainstAProperty.cs
@@ -0,0 +1,33 @@
+﻿using Checkk.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace Checkk.Tests
+{
+    public class CheckAgainstAProperty
+    {
+        [Fact]
+        public void CheckAgainstSetPropertyWorks()
+        {
+            var person = new Person
+            {
+                Name = "Chancellor Palpatine"
+            };
+
+            Should.NotThrow(() => Check.That(() => person.Name).IsNotNullOrEmpty());
+        }
+
+        [Fact]
+        public void CheckingAgainstUnsetPropertyWorks()
+        {
+            var person = new Person();
+
+            Should.Throw<InvariantShouldNotBeNullOrEmptyException>(() => Check.That(() => person.Name).IsNotNullOrEmpty());
+        }
+
+        private class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
--- src/Checkk/CheckGenericInvariant.cs
+++ src/Checkk/CheckGenericInvariant.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Linq.Expressions;
-using System.Reflection;
 using Checkk.Exceptions;
 
 namespace Checkk
 {
     /// <summary>
-    /// Methods for checking a generic instance of T
+    ///     Methods for checking a generic instance of T
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class CheckGenericInvariant<T>
@@ -18,9 +17,25 @@ namespace Checkk
             Target = target;
         }
 
+        protected T TargetValue => Target.Compile()();
+
+        protected string FieldName
+        {
+            get
+            {
+                var body = Target.Body as MemberExpression;
+                if (body != null)
+                {
+                    return body.Member.Name;
+                }
+
+                throw new ArgumentException($"Cannot get field name for expression '{Target}', the target body is {Target.Body.GetType()}");
+            }
+        }
+
         /// <summary>
-        /// Check that the target value is not null.
-        /// Throws an InvariantShouldNotBeNullException if the target value is null.
+        ///     Check that the target value is not null.
+        ///     Throws an InvariantShouldNotBeNullException if the target value is null.
         /// </summary>
         /// <param name="message">An optional message that overrides the automatically generated one if the check fails</param>
         public void IsNotNull(string message = null)
@@ -28,32 +43,6 @@ namespace Checkk
             if (object.ReferenceEquals(TargetValue, null))
             {
                 throw new InvariantShouldNotBeNullException(FieldName, message);
-            }
-        }
-
-        protected T TargetValue { get { return Target.Compile()(); } }
-
-        protected string FieldName
-        {
-            get
-            {
-                var memberExpression = Target.Body as MemberExpression;
-                if (memberExpression == null)
-                {
-                    throw new ArgumentException(string.Format(
-                        "Expression '{0}' refers to a method, not a field",
-                        Target));
-                }
-
-                var fieldInfo = memberExpression.Member as FieldInfo;
-                if (fieldInfo == null)
-                {
-                    throw new ArgumentException(string.Format(
-                        "Expression '{0}' refers to a method, not a field",
-                        Target));
-                }
-
-                return fieldInfo.Name;
             }
         }
     }
